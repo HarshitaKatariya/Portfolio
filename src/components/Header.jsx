c2a3cc16fd6f7a3a29f2ac8642b0718adadecfb5
HarshitaKatariya <harshitakatariya1508@gmail.com>
2024-11-25 13:01:39 +0530

    Second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import Logo from './logo'
 import Navbar from './Navbar'
 
 function Header() {
   return (
     
     <header className='bg-transparent top-0 z-[20] mx-auto flex w-full items-center justify-between border-b-4 p-5 border-white-500 flex-wrap'>
-      <Logo />
+      <div className='logo h-20 w-20   text-slate-100 flex ' >
+        <p className='justify-center pt-5 text-red-700 text-3xl'>@H</p>
+        <p className='justify-center pt-5 text-white text-3xl'>K</p>  
+      </div>
+
       <Navbar />
-        
     </header> 
   )
 }
